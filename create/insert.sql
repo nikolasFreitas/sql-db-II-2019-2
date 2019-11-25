9257e5ab9c1ffad27eea408158218ccca765d24e
Nikolas Freitas <nikolasbvf@gmail.com>
2019-11-25 01:15:52 -0300

    :sparkles: Add insert of 'voo'

--- create/insert.sql
+++ create/insert.sql
@@ -1,3 +1,4 @@
+set serveroutput on;
 -- Insert categoria
 declare
     v_count number(2);
@@ -45,10 +46,11 @@ declare
     end;
 /
 
+-- Insert AERONAVE
 declare
     v_count number(2);
     v_id_categoria AERONAVE.ID_AERONAVE%type;
-    v_modelo AERONAVE.MODELO%type;
+    v_modelo AERONAVE.MODELO%type := 'CESNA';
     begin
         for v_count IN 1..4 loop
             if MOD(v_count,2) = 0 then
@@ -70,3 +72,96 @@ declare
         end loop;
     end;
 /
+
+-- Insert COMPANHIA AEREA
+declare
+    v_count number(2);
+    v_nome COMPANHIA_AEREA.NOME%type := 'TAM';
+    begin
+        FOR v_count in 1..3 loop
+            if v_count = 2 then
+                v_nome := 'GOL';
+            elsif v_count = 3 then
+                v_nome := 'AZUL';
+            end if;            
+            insert into companhia_aerea (id_companhia_aerea, nome) 
+                values (sCompAerea.nextval, v_nome );
+        end LOOP;
+    end;
+    /
+    
+-- Insert FROTA
+declare
+    var_id_aeronave FROTA.ID_AERONAVE%type;
+    var_id_companhia_aerea FROTA.ID_COMPANHIA_AEREA%type;
+    CURSOR cAirComp is
+        select id_aeronave from aeronave;
+        
+    CURSOR cCompanhia is 
+        select id_companhia_aerea from companhia_aerea;
+        
+    begin
+        for iAir IN cAirComp loop
+            for iCompanhia IN cCompanhia LOOP
+            var_id_aeronave := iAir.ID_AERONAVE;
+            var_id_companhia_aerea := iCompanhia.ID_COMPANHIA_AEREA;
+                IF var_id_companhia_aerea = 3 and iAir.ID_AERONAVE = 2 then
+                    var_id_aeronave := 3;
+                end if;
+                IF var_id_companhia_aerea = 1 then
+                    var_id_aeronave := 2;
+                end if;
+                insert into frota (id_callsign, id_aeronave, id_companhia_aerea)
+                    values (sFrota.nextval, var_id_aeronave, var_id_companhia_aerea);
+                
+            END LOOP;
+        end loop;
+    end;
+/
+
+CREATE OR REPLACE FUNCTION isPortaoValid( p_id_aeronave in number, p_id_portao in number)
+    return BOOLEAN
+    is
+        v_portao_qtd number(6);
+        v_aeronave_qtd number(6);
+    begin
+        select C.quantidade into v_portao_qtd
+            from portao p
+            inner join categoria c on C.ID_CATEGORIA = P.ID_CATEGORIA
+            where p_id_portao = P.ID_PORTAO;
+            
+        select C.quantidade into v_aeronave_qtd
+            from aeronave A
+            inner join categoria c on C.ID_CATEGORIA = A.ID_CATEGORIA
+            where p_id_aeronave = A.ID_AERONAVE;
+            
+        return v_aeronave_qtd <= v_portao_qtd;
+    end;
+    /
+
+declare
+    v_portao PORTAO.ID_PORTAO%type;
+    v_id_aeronave number(2);
+    v_id_companhia_aerea number(2);
+    v_last_id_aeronave number(2);
+    v_last_id_companhia_aerea number(2);
+    
+    CURSOR cFrota is
+        select id_aeronave, id_companhia_aerea from frota;
+        
+    CURSOR cPortao is
+        select id_portao from portao;
+    begin
+        FOR fFrota in cFrota LOOP
+            FOR fPortao in cPortao LOOP
+                IF isPortaoValid(fFrota.id_aeronave, fPortao.id_portao) then
+                    INSERT INTO VOO (id_voo, id_aeronave, id_companhia_aerea, data_chegada, date_saida)
+                        values (sVoo.nextval, fFrota.id_aeronave, fFrota.id_companhia_aerea, TO_DATE('2019/08/15 8:30:25', 'YYYY/MM/DD HH:MI:SS'), TO_DATE('2019/08/21 8:30:25', 'YYYY/MM/DD HH:MI:SS'));
+                else 
+                    DBMS_OUTPUT.PUT_LINE('Deu FALSE');
+                end if;
+            END LOOP;
+        END LOOP;
+    end;
+/
+    
