9ff6798087c169f54cc0c5ad4a03258d9d4e7d49
Nikolas Freitas <nikolasbvf@gmail.com>
2019-11-28 02:39:23 -0300

    Under construction

--- procedure/jdbc-select.sql
+++ procedure/jdbc-select.sql
@@ -1,40 +1,82 @@
+set serveroutput on;
+-- Verifiacar quais portões estão disponíveis
+CREATE or replace procedure pegaPortoesDisponiveis(p_data_voo in voo.data_saida%type, 
+    p_quantidade in number, 
+    p_cos out SYS_REFCURSOR)
+    is
+    begin
+        open p_cos for
+        select DISTINCT p.* from portao p
+            inner join categoria c on c.ID_CATEGORIA = P.ID_CATEGORIA
+            inner join voo v on v.id_portao = p.id_portao
+            where p.DISPONIBILIDADE != 0 
+                and C.QUANTIDADE >= p_quantidade 
+                and V.DATA_SAIDA <> p_data_voo 
+                and v.data_chegada <> p_data_voo;
+    end;
+/
+
 -- Verificar quais companhia aéreas podem fazer o vôo numa data especificae com modelo de avião específico
 -- e quais portões estão disponíveis
 CREATE OR REPLACE PROCEDURE selecionaPortoesDisponiveis(p_data_voo in voo.data_saida%type, 
-    p_modelo_aeronave in AERONAVE.MODELO%type)
-    --p_recordset OUT SYS_REFCURSOR) 
+    p_modelo_aeronave in AERONAVE.MODELO%type,
+    v_rc out sys_refcursor) 
     IS
-        v_quantidade_aeronave number;
+        TYPE tableTemplate IS TABLE OF portao%type;
+        table_portao tableTemplate;
         
-        cursor cPortao(c_quantidade number,c_data_saida date) is
-            select p.* from portao p
+        v_quantidade_aeronave number;
+        cursor cPortao(c_data_voo date, c_quantidade number) is
+        select p.* from portao p
             inner join categoria c on c.ID_CATEGORIA = P.ID_CATEGORIA
             inner join voo v on v.id_portao = p.id_portao
             where p.DISPONIBILIDADE != 0 
                 and C.QUANTIDADE >= c_quantidade 
-                and V.DATA_SAIDA <> c_data_saida 
-                and v.data_chegada <> C_DATA_SAIDA;
-            
-        cursor cFrota (c_modelo varchar2) is
-            select f.* from frota f 
-            inner join aeronave a on a.id_aeronave = f.id_aeronave
-            where a.modelo = c_modelo;
+                and V.DATA_SAIDA <> c_data_voo 
+                and v.data_chegada <> c_data_voo;
+                
     begin
         select c.quantidade into v_quantidade_aeronave from aeronave a
             inner join categoria c on c.id_categoria = A.ID_CATEGORIA
             where A.MODELO = P_MODELO_AERONAVE;
-            
-        for rPortao IN cPortao(v_quantidade_aeronave, P_DATA_VOO) loop
-            for rFrota in cFrota(p_modelo_aeronave) loop
-                DBMS_OUTPUT.PUT_LINE(rFrota.id_companhia_aerea);
+            open cPortao(p_data_voo, v_quantidade_aeronave);
+            loop
+                fetch cPortao bulk collect into table_portao;
+                EXIT WHEN table_portao.COUNT = 0;
             end loop;
-        end loop;
-        
-        --OPEN p_recordset FOR
-           -- SELECT * FROM PORTAO
-               -- WHERE DISPONIBILIDADE != 0;
+            close cPortao;
+
+            open v_rc for table_portao;
+
+        --for lPortao IN cPortao(p_data_voo, v_quantidade_aeronave) loop            
+          --      open v_rc for select distinct ca.*, p.* from companhia_aerea ca 
+          --      inner join frota f on F.ID_COMPANHIA_AEREA = CA.ID_COMPANHIA_AEREA
+           --     inner join AERONAVE A on A.ID_AERONAVE = F.ID_AERONAVE
+           --     inner join categoria c on C.ID_CATEGORIA = A.ID_CATEGORIA
+             --   inner join portao p on P.ID_CATEGORIA = C.ID_CATEGORIA
+               -- where P.ID_PORTAO = lPortao.id_portao;
+        --end loop;    
+         DBMS_SQL.RETURN_RESULT(v_rc);
     end;
 /
 
-call selecionaPortoesDisponiveis(TO_DATE('2019/08/16 8:30:25', 'YYYY/MM/DD HH:MI:SS'), 'boeing 737');
+CREATE OR REPLACE PROCEDURE selecionaPortoesDisponiveis(
+    p_data_voo in voo.data_saida%type, 
+    p_modelo_aeronave in AERONAVE.MODELO%type)      
+    is
+        v_rc SYS_REFCURSOR;
+        cursorzin refcursor;
+    begin
+        pegaPortoesDisponiveis(p_data_voo, 30, v_rc);        
+        
+    end;
+/
+
+var cursorzin refcursor;
+exec pegaPortoesDisponiveis(TO_DATE('2019/08/16 8:30:25', 'YYYY/MM/DD HH:MI:SS'), 30, :cursorzin); 
+print cursorzin;
+
+var otro refcursor;
+EXECUTE selecionaPortoesDisponiveis(TO_DATE('2019/08/16 8:30:25', 'YYYY/MM/DD HH:MI:SS'), 'King air', :otro);
+print otro;
 
